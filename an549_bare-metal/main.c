db8f7e9e9bcdf25617055c9432f3443e196adc00
Gabriel Wang <gabriel.wang@arm.com>
2022-06-30 00:33:47 +0100

    Update main.c

--- an549_bare-metal/main.c
+++ an549_bare-metal/main.c
@@ -59,7 +59,7 @@ static void test(void)
 #if LV_USE_DEMO_BENCHMARK
 static void on_benchmark_finished(void)
 {
-    disp_enable();
+    disp_enable_update();
 }
 #endif
 
@@ -81,12 +81,12 @@ int main(void)
     __LL_LCD_PRINT(25, 0, "Please stand by...");
     __LL_LCD_PRINT(28, 0, "NOTE: You will NOT see anything until the end.");
 
-    disp_disable();
+    disp_disable_update();
     
     lv_demo_benchmark_set_finished_cb(&on_benchmark_finished);
     lv_demo_benchmark();
     
-    //lv_demo_benchmark_run_scene(43);      // run specified scence
+    //lv_demo_benchmark_run_scene(85);      // run specified scence
     
 #elif LV_USE_DEMO_WIDGETS
     lv_demo_widgets();

07a9595257b7854dad0590cb3139ce505c8fa881
Gabriel Wang <gabriel.wang@arm.com>
2022-10-03 01:15:56 +0100

    Update lv_conf_cmsis.h

--- an549_bare-metal/project/mdk/RTE/LVGL/lv_conf_cmsis.h
+++ an549_bare-metal/project/mdk/RTE/LVGL/lv_conf_cmsis.h
@@ -201,7 +201,7 @@
  *-----------*/
 
 /*Enable the log module*/
-#define LV_USE_LOG 0
+#define LV_USE_LOG 1
 #if LV_USE_LOG
 
     /*How important log should be added:
@@ -211,11 +211,11 @@
     *LV_LOG_LEVEL_ERROR       Only critical issue, when the system may fail
     *LV_LOG_LEVEL_USER        Only logs added by the user
     *LV_LOG_LEVEL_NONE        Do not log anything*/
-    #define LV_LOG_LEVEL LV_LOG_LEVEL_WARN
+    #define LV_LOG_LEVEL LV_LOG_LEVEL_USER
 
     /*1: Print the log with 'printf';
     *0: User need to register a callback with `lv_log_register_print_cb()`*/
-    #define LV_LOG_PRINTF 0
+    #define LV_LOG_PRINTF 1
 
     /*1: Enable print timestamp;
      *0: Disable print timestamp*/
